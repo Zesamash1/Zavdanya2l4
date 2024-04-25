935e245cd29dfd14aa39dc944f2b86e808cc723d
ASUS <ASUS@ZESAMASHI>
2024-04-25 18:24:23 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp6/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show("Заповніть всі поля.", "Помилка.");
+            }
+            else
+            {
+                int n = dataGridView1.Rows.Add();
+                dataGridView1.Rows[n].Cells[0].Value = textBox1.Text;
+                dataGridView1.Rows[n].Cells[1].Value = numericUpDown1.Value;
+                dataGridView1.Rows[n].Cells[2].Value = comboBox1.Text;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                dataGridView1.Rows.RemoveAt(dataGridView1.SelectedRows[0].Index);
+            }
+            else
+            {
+                MessageBox.Show("Оберіть рядок для видалення.", "Помилка.");
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            textBox1.Text = dataGridView1.SelectedRows[0].Cells[0].Value.ToString();
+            int n = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells[1].Value);
+            numericUpDown1.Value = n;
+            comboBox1.Text = dataGridView1.SelectedRows[0].Cells[2].Value.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                int n = dataGridView1.SelectedRows[0].Index;
+                dataGridView1.Rows[n].Cells[0].Value = textBox1.Text;
+                dataGridView1.Rows[n].Cells[1].Value = numericUpDown1.Value;
+                dataGridView1.Rows[n].Cells[2].Value = comboBox1.Text;
+            }
+            else
+            {
+                MessageBox.Show("Оберіть рядок для редагування", "Помилка.");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DataSet ds = new DataSet();
+                DataTable dt = new DataTable();
+                dt.TableName = "Employee";
+                dt.Columns.Add("Name");
+                dt.Columns.Add("Age");
+                dt.Columns.Add("Programmer");
+                ds.Tables.Add(dt);
+                foreach (DataGridViewRow r in dataGridView1.Rows)
+                {
+                    DataRow row = ds.Tables["Employee"].NewRow();
+                    row["Name"] = r.Cells[0].Value;
+                    row["Age"] = r.Cells[1].Value;
+                    row["Programmer"] = r.Cells[2].Value;
+                    ds.Tables["Employee"].Rows.Add(row);
+                }
+                ds.WriteXml("F:\\Data.xml");
+                MessageBox.Show("XML файл успішно збережений.", "Виконано.");
+            }
+            catch
+            {
+                MessageBox.Show("Неможливо зберегти XML файл.", "Помилка.");
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.Rows.Count > 1) // Замість нуля поставив 1 бо при запускі програми автоматично з'являється перший рядок
+                                              // і тому команда не працює
+            {
+                MessageBox.Show("Очистіть поле перед завантаженням нового файла.", "Помилка.");
+            }
+            else
+            {
+                if (File.Exists("F:\\Data.xml"))
+                {
+                    DataSet ds = new DataSet();
+                    ds.ReadXml("F:\\Data.xml");
+                    foreach (DataRow item in ds.Tables["Employee"].Rows)
+                    {
+                        int n = dataGridView1.Rows.Add();
+                        dataGridView1.Rows[n].Cells[0].Value = item["Name"];
+                        dataGridView1.Rows[n].Cells[1].Value = item["Age"];
+                        dataGridView1.Rows[n].Cells[2].Value = item["Programmer"];
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("XML файл не знайдено.", "Помилка.");
+                }
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.Rows.Count > 0)
+            {
+                dataGridView1.Rows.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Таблиця порожня.", "Помилка.");
+            }
+        }
+    }
+}
